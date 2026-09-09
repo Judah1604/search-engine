8ab87da285eb5603378c1bcee6bf9c9a60a62bef
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2026-09-09 04:48:39 +0100

    feat: multi-word sliding-window search with sorted results

--- main.js
+++ main.js
@@ -2,21 +2,44 @@ import fs from "fs";
 
 const files = fs.readdirSync("./files");
 
-const query = "It";
+const queryArray = process.argv.slice(2, process.argv.length);
+const query = queryArray.join(" ").toLowerCase().trim();
+console.log(query);
+let results = [];
 
 for (let index = 0; index < files.length; index++) {
 	let occurences = 0;
-	const cleanedQuery = query.trim().toLowerCase();
-    const fileName = files[index]
-	const fileContent = fs.readFileSync(`./files/${fileName}`, 'utf8');
+	const fileName = files[index];
+	const fileContent = fs.readFileSync(`./files/${fileName}`, "utf8");
 	const words = fileContent.trim().toLowerCase().split(/\W+/).filter(Boolean);
-    console.log(words)
+	const chunks = [];
 
-	for (let index = 0; index < words.length; index++) {
-		if (words[index] === cleanedQuery) {
-			occurences++;
+	if (queryArray.length === 1) {
+		for (let index = 0; index < words.length; index++) {
+			if (words[index] === query) {
+				occurences++;
+			}
+		}
+	} else {
+		for (let index = 0; index < words.length - queryArray.length; index++) {
+			let chunk = words.slice(index, index + queryArray.length).join(" ");
+			if (chunk === query) {
+				occurences++;
+			}
 		}
 	}
 
-    console.log(fileName, occurences + ' times')
+	results.push({ fileName: fileName, occurences: occurences });
+}
+
+results = results
+	.filter((result) => result.occurences !== 0)
+	.sort((a, b) => b.occurences - a.occurences);
+
+if (results.length === 0) {
+	console.log("Found no results.");
+} else {
+	results.forEach((result) =>
+		console.log(result.fileName, "found " + result.occurences + " times"),
+	);
 }
