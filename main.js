f1beb739b8dbc64591888f3aada54fbb2cbf89f5
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2026-09-10 13:20:57 +0100

    experimented structure for inverted index

--- main.js
+++ main.js
@@ -1,46 +1,61 @@
-import fs from "fs";
+// import fs from "fs";
 
-const files = fs.readdirSync("./files");
+// const files = fs.readdirSync("./files");
 
-const queryArray = process.argv.slice(2, process.argv.length);
-const query = queryArray.join(" ").toLowerCase().trim();
-console.log(query);
-let results = [];
+// const queryArray = process.argv.slice(2, process.argv.length);
+// const query = queryArray.join(" ").toLowerCase().trim();
+// console.log(query);
+// let results = [];
 
-for (let index = 0; index < files.length; index++) {
-	let occurences = 0;
-	const fileName = files[index];
-	const fileContent = fs.readFileSync(`./files/${fileName}`, "utf8");
-	const words = fileContent.trim().toLowerCase().split(/\W+/).filter(Boolean);
+// for (let index = 0; index < files.length; index++) {
+// 	let occurences = 0;
+// 	const fileName = files[index];
+// 	const fileContent = fs.readFileSync(`./files/${fileName}`, "utf8");
+// 	const words = fileContent.trim().toLowerCase().split(/\W+/).filter(Boolean);
 
-	if (queryArray.length === 1) {
-		for (let index = 0; index < words.length; index++) {
-			if (words[index] === query) {
-				occurences++;
-			}
-		}
-	} else {
-		for (let index = 0; index < words.length - queryArray.length; index++) {
-			let chunk = words.slice(index, index + queryArray.length).join(" ");
-			if (chunk === query) {
-				occurences++;
-			}
-		}
-	}
+// 	if (queryArray.length === 1) {
+// 		for (let index = 0; index < words.length; index++) {
+// 			if (words[index] === query) {
+// 				occurences++;
+// 			}
+// 		}
+// 	} else {
+// 		for (let index = 0; index < words.length - queryArray.length; index++) {
+// 			let chunk = words.slice(index, index + queryArray.length).join(" ");
+// 			if (chunk === query) {
+// 				occurences++;
+// 			}
+// 		}
+// 	}
 
-	results.push({ fileName: fileName, occurences: occurences });
-}
+// 	results.push({ fileName: fileName, occurences: occurences });
+// }
 
-results = results
-	.filter((result) => result.occurences !== 0)
-	.sort((a, b) => b.occurences - a.occurences);
+// results = results
+// 	.filter((result) => result.occurences !== 0)
+// 	.sort((a, b) => b.occurences - a.occurences);
 
-if (results.length === 0) {
-	console.log("Found no results.");
+// if (results.length === 0) {
+// 	console.log("Found no results.");
+// } else {
+// 	results.forEach((result) =>
+// 		console.log(result.fileName, "found " + result.occurences + " times"),
+// 	);
+// }
+
+let index = {
+	word: { fileNames: ["doc1", "doc2"] },
+	quantum: { fileNames: ["doc1", "doc3"] },
+};
+
+let query = "quantum";
+
+if (query in index) {
+	console.log("found " + query);
+    const item = index[query]
+    
+
+
 } else {
-	results.forEach((result) =>
-		console.log(result.fileName, "found " + result.occurences + " times"),
-	);
+	console.log(query + " not found");
 }
-
-
