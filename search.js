d5a1db831a5230cea6086494287633c525928d48
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2026-09-11 20:32:56 +0100

    integrated single and multiword queries

--- search.js
+++ search.js
@@ -1,14 +1,17 @@
 import fs from "fs";
 
 const fileIndex = JSON.parse(fs.readFileSync("./fileIndex.json", "utf-8"));
-const queryArray = process.argv.slice(2, process.argv.length);
-let matchedFiles = [];
+let queryArray = process.argv.slice(2, process.argv.length);
+let limit = queryArray.length;
+queryArray.forEach((query) => queryArray.push(query.toLowerCase()));
+queryArray.splice(0, limit);
 
 if (fileIndex[queryArray[0]] === undefined) {
 	console.log("Query not found");
 	process.exit();
 }
 
+let sharedFilesSingleQuery = fileIndex[queryArray[0]].files;
 let sharedFiles = Object.keys(fileIndex[queryArray[0]].files);
 
 if (queryArray.length > 1) {
@@ -26,18 +29,22 @@ if (queryArray.length > 1) {
 		);
 	}
 	for (let index = 0; index < sharedFiles.length; index++) {
-		let word1Positions = fileIndex[queryArray[0]].files[sharedFiles[index]];
-		let word2Positions = fileIndex[queryArray[1]].files[sharedFiles[index]];
-
-		for (let posIndex = 0; posIndex < word1Positions.length; posIndex++) {
-			if (word2Positions.includes(word1Positions[posIndex] + 1)) {
-				matchedFiles.push(sharedFiles[index]);
-				break;
+		let count = 0;
+		for (let i = 0; i < queryArray.length - 1; i++) {
+			let wordPos = fileIndex[queryArray[i]].files[sharedFiles[index]];
+			let nextWordPos =
+				fileIndex[queryArray[i + 1]].files[sharedFiles[index]];
+			for (let posIndex = 0; posIndex < wordPos.length; posIndex++) {
+				if (nextWordPos.includes(wordPos[posIndex] + 1)) {
+					count++;
+				}
 			}
 		}
+        console.log(sharedFiles[index], "found", count, "times");
 	}
 } else {
-	console.log(sharedFiles);
+	const fileArray = Object.entries(sharedFilesSingleQuery);
+	fileArray.forEach(([fileName, occurrences]) => {
+		console.log(fileName, "found " + occurrences.length + " times");
+	});
 }
-
-console.log(matchedFiles);
